52f196fd623850b5c3b773aaf85497991468770d
Pushpraj733 <114551439+Pushpraj733@users.noreply.github.com>
2024-12-24 16:18:25 +0530

    implement stripe

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -5,14 +5,19 @@ import { formatDateTime } from '@/lib/utils';
 import { SearchParamProps } from '@/types'
 import Image from 'next/image';
 
-const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) => {
+const EventDetails = async ({
+  params,
+  searchParams,
+}: SearchParamProps) => {
+  const { id } = await Promise.resolve(params);
+
   const event = await getEventById(id);
 
   const relatedEvents = await getRelatedEventsByCategory({
     categoryId: event.category._id,
     eventId: event._id,
-    page: searchParams.page as string,
-  })
+    page: searchParams?.page as string,
+  });
 
   return (
     <>
@@ -89,7 +94,7 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
           emptyStateSubtext="Come back later"
           collectionType="All_Events"
           limit={3}
-          page={searchParams.page as string}
+          page={searchParams?.page as string || "1"}
           totalPages={relatedEvents?.totalPages}
         />
     </section>
